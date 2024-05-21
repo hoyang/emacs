2587f4be1e52f88431abbccda995d8a60f96841a
H.Yang <hoyang@MachineLearning>
2024-05-21 13:20:53 +0800

    remove useless

--- init.el
+++ init.el
@@ -25,20 +25,4 @@
 		"hotfix.el"
 		"workspace.el"
 		"test.el"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(helm-gtags-prefix-key "\24")
- '(helm-gtags-suggested-key-mapping t)
- '(markdown-command "/usr/local/bin/markdown")
- '(package-selected-packages
-   '(yasnippet-snippets yaml-mode web-mode typescript-mode treepy tide swift-mode solidity-flycheck shut-up rust-mode restclient php-mode monokai-theme magit-popup magit lua-mode lsp-ui kotlin-mode helm-projectile helm-lsp helm-gtags helm-company go-mode gdscript-mode find-file-in-project erlang epc elpy dockerfile-mode dap-mode cquery company-statistics company-solidity company-quickhelp company-box cmake-mode cmake-ide))
- '(typescript-indent-level 2))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
